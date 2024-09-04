8d068f0b97e86a8fefba2aa1d432edc2e1357698
James Lim <jameslimbj@gmail.com>
2024-09-04 21:30:44 +0800

    :rocket: The start of something beautiful

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import { FaGithub, FaInstagram, FaLinkedin } from 'react-icons/fa';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>
+          Hi! I'm<br />
+          <span className="highlight">James</span>
+        </h1>
+        <p className="subtitle">Servant of God, slave to C++</p>
+        <div className="social-links">
+          <a href="https://github.com/lim-james" target="_blank" rel="noopener noreferrer">
+            <FaGithub />
+          </a>
+          <a href="https://www.instagram.com/jamesl.im/" target="_blank" rel="noopener noreferrer">
+            <FaInstagram />
+          </a>
+          <a href="https://www.linkedin.com/in/james-lim-557bb0153/" target="_blank" rel="noopener noreferrer">
+            <FaLinkedin />
+          </a>
+        </div>
       </header>
     </div>
   );
 }
 
 export default App;
+
